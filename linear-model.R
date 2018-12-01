df851e93a7dc732f8da496c1c8b9572737c9b92b
Michael Fedell <michaelfedell14@gmail.com>
2018-11-30 18:08:41 -0600

    Complete linear-model (ml.best) and validate against test data

--- linear-model.R
+++ linear-model.R
@@ -1,6 +1,6 @@
-source('./loader.R')
+source('./loader.R', chdir = T)
 train.full <- train.full %>% select(id, logtargamt, recency, frequency, amount, tof, respond)
-train.full <- train.full %>% mutate(ordersper = frequency / tof, amountper = amount / tof)
+train.full <- train.full %>% mutate(ordersPer = frequency / tof, amountPer = amount / tof)
 orders <- orders %>% filter(qty < 1000)
 orders <- orders %>% mutate(returned = ifelse(price == 0, 1, 0))
 train.orders <- merge(train.full, orders, by = 'id')
@@ -10,9 +10,19 @@ train.full <- train.full %>%
           summarise(sumQty = sum(qty),
                     maxPrice = max(price),
                     maxNet = max(net),
+                    returned = max(returned),
                     oneMonth = max(oneMonth),
                     threeMonth = max(threeMonth),
                     sixMonth = max(sixMonth),
                     oneYear = max(oneYear),
                     overYear = max(overYear)),
         by = 'id')
+train.responders <- train.full %>% filter(respond == 1, tof > 0)
+train.responders <- train.responders[-c(55, 65, 68, 70, 131, 171, 256), ]
+ml.baseline <- lm(logtargamt ~ recency + frequency + amount + tof + 
+                 ordersPer + amountPer + sumQty + maxPrice + returned + 
+                 oneMonth + threeMonth + sixMonth + oneYear + overYear, 
+               data=train.responders)
+step(baseline, direction = "both")
+ml.best <- lm(formula = logtargamt ~ recency + frequency + amount + amountPer + 
+             sumQty + oneMonth, data = train.responders)
--- loader.R
+++ loader.R
@@ -11,6 +11,7 @@ book$respond <- ifelse(book$logtargamt > 0, 1, 0)
 
 # Drop empty malformat column
 train$X <- NULL
+test$X <- NULL
 
 # Orders with orddate as Date and net as total price of order
 orders$orddate <- lubridate::dmy(orders$orddate)
@@ -26,11 +27,10 @@ train.merge <- merge(train, book, by = 'id')
 train.merge$logtargamt.y <- NULL
 train.merge <- rename(train.merge, logtargamt = logtargamt.x)
 
-# All information about customers in training set with addition of avgNetOrder
-train.full <- orders %>% 
-  group_by(id) %>% 
-  summarise(avgNetOrder = mean(net)) %>%
-  merge(train.merge, by = 'id', all.y = T)
+# All information about customers in the test set
+test.merge <- merge(test, book, by = 'id')
+test.merge$logtargamt.y <- NULL
+test.merge <- rename(test.merge, logtargamt = logtargamt.x)
 
 # 
 startDate <- as.Date('01-Aug-14', '%d-%B-%y')
@@ -42,3 +42,15 @@ orders <- orders %>% mutate(
   oneYear = ifelse(dateDiff < 365 & dateDiff > 183, 1, 0),
   overYear = ifelse(dateDiff > 365, 1, 0)
 )
+
+# All information about customers in training set with addition of avgNetOrder
+train.full <- orders %>% 
+  group_by(id) %>% 
+  summarise(avgNetOrder = mean(net)) %>%
+  merge(train.merge, by = 'id', all.y = T)
+
+# All information about customers in test set with addition of avgNetOrder
+test.full <- orders %>% 
+  group_by(id) %>% 
+  summarise(avgNetOrder = mean(net)) %>%
+  merge(test.merge, by = 'id', all.y = T)
