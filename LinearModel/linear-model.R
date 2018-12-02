44488145382bc51f6f62424b833e1022969db365
Michael Fedell <michaelfedell14@gmail.com>
2018-12-02 15:12:16 -0600

    Evaluate financial criterion

--- LinearModel/linear-model.R
+++ LinearModel/linear-model.R
@@ -1,10 +1,12 @@
 source('../loader.R', chdir = T)
+# Subset training set to data relevant for multiple regression
 train.responders <- train.full %>% filter(respond == 1, tof > 0)
 train.responders <- train.responders[-c(55, 65, 68, 70, 131, 171, 256), ]
 ml.baseline <- lm(logtargamt ~ recency + frequency + amount + tof + 
                  ordersPer + amountPer + sumQty + maxPrice + returned + 
                  oneMonth + threeMonth + sixMonth + oneYear + overYear, 
                data=train.responders)
-# step(ml.baseline, direction = "both")
+
+# Train best multiple regression model (result of stepwise regression and iteration)
 ml.best <- lm(formula = logtargamt ~ recency + frequency + amount + amountPer + 
              sumQty + oneMonth, data = train.responders)
--- LogisticModel/logistic-model.R
+++ LogisticModel/logistic-model.R
@@ -1,10 +1,31 @@
-source('../loader.R')
+source('../loader.R', chdir = T)
+# Subset training set to data relevant for logistic regression
 train.logistic <- train.full %>% 
-  select(id, avgNetOrder, sumQty, logtargamt, sumQtyPerTof,
-         recency, frequency, amount, amountPer, tof, respond,
+  select(id, respond, avgNetOrder, sumQty, logtargamt, sumQtyPerTof,
+         recency, frequency, amount, amountPer, tof,
          oneMonth, threeMonth, sixMonth, oneYear, overYear) %>%
-  filter(tof > 0)
+  filter(tof > 0, sumQty < 1000)
 
+log.baseline = glm(respond ~ avgNetOrder + sumQty + recency + frequency + amount + tof, 
+                 data = train.logistic, family = binomial)
+# Remove outliers by Cook's Distance
+train.logistic <- train.logistic %>% 
+  filter(cooks.distance(log.baseline) < qf(0.1,6,8218))
+
+# Oversample responders for training
+prob1 <- ifelse(train.logistic$respond == 0,
+                0.85/(length(which(train.logistic$respond == 0))),
+                0.15/(length(which(train.logistic$respond == 1))))
+set.seed(123)
+train.rebalanced <- train.logistic[sample(c(1:nrow(train.logistic)), 
+                                          replace = TRUE, prob = prob1),]
+length(which(train.rebalanced$respond == 1))
+
+# Note number of folds by which responders were oversampled
+m <- (nrow(filter(train.rebalanced, respond == 1)) / nrow(train.rebalanced)) / 
+  (nrow(filter(train.full, respond == 1)) / nrow(train.full))
+
+# Train best logistic model (result of stepwise regression and iteration)
 log.best <- glm(formula = respond ~ avgNetOrder + sumQty + recency + frequency + 
                   tof + amountPer + oneMonth + threeMonth + overYear, family = binomial, 
                 data = train.rebalanced)
