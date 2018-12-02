30303937fb0bf84d2cabd1b0910db74d68924cff
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2018-12-01 23:52:30 -0600

    Complete logistic model and updated in the predictions

--- LogisticModel/logistic-model.R
+++ LogisticModel/logistic-model.R
@@ -1,10 +1,10 @@
 source('../loader.R')
 train.logistic <- train.full %>% 
   select(id, avgNetOrder, sumQty, logtargamt, sumQtyPerTof,
-         recency, frequency, amount, tof, respond,
+         recency, frequency, amount, amountPer, tof, respond,
          oneMonth, threeMonth, sixMonth, oneYear, overYear) %>%
   filter(tof > 0)
 
-log.best <- glm(respond ~ avgNetOrder + sumQty + recency + frequency  + tof + 
-                  oneMonth + threeMonth + oneYear + sumQtyPerTof,
-                data = train.rebalanced, family = binomial)
+log.best <- glm(formula = respond ~ avgNetOrder + sumQty + recency + frequency + 
+                  tof + amountPer + oneMonth + threeMonth + overYear, family = binomial, 
+                data = train.rebalanced)
