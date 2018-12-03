c06b55940a183f708a3e393a9e7db6ffb844c697
Michael Fedell <michaelfedell14@gmail.com>
2018-12-03 08:15:43 -0600

    correct linear model

--- LinearModel/linear-model.R
+++ LinearModel/linear-model.R
@@ -4,7 +4,7 @@ train.responders <- train.full %>%
   select(id, logtargamt, recency, frequency, amount, amountPer, tof, ordersPer, sumQty, 
          maxPrice, returned, oneMonth, threeMonth, sixMonth, oneYear, overYear) %>% 
   filter(logtargamt > 0, tof > 0)
-# train.responders <- train.responders[-c(55, 65, 68, 70, 131, 171, 256), ]
+
 ml.baseline <- lm(logtargamt ~ recency + frequency + amount + tof + 
                  ordersPer + amountPer + sumQty + maxPrice + returned + 
                  oneMonth + threeMonth + sixMonth + oneYear + overYear, 
@@ -15,5 +15,5 @@ train.responders <- train.responders %>%
   filter(cooks.distance(ml.baseline) < qf(0.1, p + 1, nrow(train.responders) - p + 1))
 
 # Train best multiple regression model (result of stepwise regression and iteration)
-ml.best <- lm(formula = logtargamt ~ frequency + amount + amountPer + sumQty,
+ml.best <- lm(formula = logtargamt ~ frequency + amountPer + sumQty + maxPrice + oneMonth,
               data = train.responders)
