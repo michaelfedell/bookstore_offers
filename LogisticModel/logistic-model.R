ef5dcb96322d6a331818f488c09cbcf4f4f31b27
Michael Fedell <michaelfedell14@gmail.com>
2018-12-02 19:03:40 -0600

    Update logistic model with new predictors and resampling

--- LogisticModel/logistic-model.R
+++ LogisticModel/logistic-model.R
@@ -1,31 +1,35 @@
 source('../loader.R', chdir = T)
 # Subset training set to data relevant for logistic regression
 train.logistic <- train.full %>% 
-  select(id, respond, avgNetOrder, sumQty, logtargamt, sumQtyPerTof,
-         recency, frequency, amount, amountPer, tof,
-         oneMonth, threeMonth, sixMonth, oneYear, overYear) %>%
-  filter(tof > 0, sumQty < 1000)
-
-log.baseline = glm(respond ~ avgNetOrder + sumQty + recency + frequency + amount + tof, 
-                 data = train.logistic, family = binomial)
-# Remove outliers by Cook's Distance
-train.logistic <- train.logistic %>% 
-  filter(cooks.distance(log.baseline) < qf(0.1,6,8218))
+  select(id, avgNetOrder, sumQty, logtargamt, sumQtyPerTof,
+         recency, frequency, amount, tof, respond, returned,
+         oneMonth, threeMonth, sixMonth, oneYear, overYear, amountPer, maxNet) %>%
+  filter(tof > 0)
 
 # Oversample responders for training
-prob1 <- ifelse(train.logistic$respond == 0,
-                0.85/(length(which(train.logistic$respond == 0))),
-                0.15/(length(which(train.logistic$respond == 1))))
-set.seed(123)
-train.rebalanced <- train.logistic[sample(c(1:nrow(train.logistic)), 
-                                          replace = TRUE, prob = prob1),]
+train.logistic.res <- train.logistic[train.logistic$respond ==1,]
+train.logistic.fold <- train.logistic.res[rep(seq(nrow(train.logistic.res)), 3),]
+train.rebalanced <- rbind(train.logistic.fold,train.logistic)
 length(which(train.rebalanced$respond == 1))
+
+log.baseline <-  glm(respond ~ avgNetOrder + sumQty + recency + frequency  + tof + 
+                       amount + amountPer + oneMonth + threeMonth + sixMonth + 
+                       oneYear + overYear + sumQtyPerTof + returned + maxNet,
+                     data = train.rebalanced, family = binomial)
+
+log.baseline.p <- length(log.baseline$coefficients)
+
+# Remove outliers by Cook's Distance
+train.rebalanced <- train.rebalanced %>% 
+  filter(cooks.distance(log.baseline) < qf(0.1,
+                                           log.baseline.p,
+                                           nrow(train.rebalanced) - log.baseline.p + 1))
 
 # Note number of folds by which responders were oversampled
 m <- (nrow(filter(train.rebalanced, respond == 1)) / nrow(train.rebalanced)) / 
-  (nrow(filter(train.full, respond == 1)) / nrow(train.full))
+  (nrow(filter(train.logistic, respond == 1)) / nrow(train.logistic))
 
 # Train best logistic model (result of stepwise regression and iteration)
-log.best <- glm(formula = respond ~ avgNetOrder + sumQty + recency + frequency + 
-                  tof + amountPer + oneMonth + threeMonth + overYear, family = binomial, 
-                data = train.rebalanced)
+log.best <- glm(respond ~ recency + frequency  + tof + amount +
+                  oneMonth + threeMonth + overYear + maxNet +sumQtyPerTof,
+                data = train.rebalanced, family = binomial)
